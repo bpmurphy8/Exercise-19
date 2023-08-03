6da150cefc9874ed6fb968620d73c78127270a08
Brendan Murphy <88051756+bpmurphy8@users.noreply.github.com>
2023-08-02 18:46:41 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Greetings from './Greetings';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello! Welcome to React! </h1>
+      <Greetings/>
+
+
+
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Greetings.js
@@ -0,0 +1,5 @@
+const Greetings = () => {
+    return (<h1> Welcome to my project! </h1>);
+}
+ 
+export default Greetings;
